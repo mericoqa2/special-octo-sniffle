011c669fee5ccbf0ecf173de6674c708d77c43b8
mericoqa3 <76010241+mericoqa3@users.noreply.github.com>
2020-12-14 11:49:39 -0800

    Add files via upload

---
+++ loc-10-eloc-61.js
@@ -0,0 +1,11 @@
+function my1(){alert(1)}
+function my2(){alert(2)}
+function my3(){alert(3)}
+function my4(){alert(4)}
+function my5(){alert(5)}
+function my6(){alert(6)}
+function my7(){alert(7)}
+function my8(){alert(8)}
+function my9(){alert(9)}
+function my10(){alert(10)}
+
